a11a5639a532e9c36425447c0eca2645d54d7c73
IDsebin <94587782+IDsebin@users.noreply.github.com>
2022-05-25 17:33:28 +0900

    6주차

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// 커멘트대로 먼저 파일 만들기 전에 branch 만들고 거기에 add 하려고 했는데 nothing to commit이 떠서 일단은 주석 처리로 commit 했습다.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React,{Component , useState ,useEffect} from 'react';
+import './App.css';
+import List from './List.js';
+import Header from './Header.js';
+import styled from 'styled-components';
+import Button from './Button';
+
+const AppBlock = styled.div`
+  width: 100px;
+  position: absolute;
+  margin-top:-23px;
+  left: 190px;
+  padding: 0px;
+
+`
+const App = () => {
+  const [todos,setTodos] = useState([0]);
+  const [newTodo, setNewTodo] = useState();
+
+
+  const changeInputData = (e) =>{
+    setNewTodo(e.target.value);
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos,newTodo]);
+  }
+
+  useEffect( () => { 
+    console.log("확인",todos);
+  },[todos])
+  
+  const fetchInitialData = async () => {
+    const response = await fetch();
+    const initialData = await response.json();
+    setTodos(initialData)
+  }
+  return(
+    <>
+    <Header todos = {todos}/>
+    <form action = "">
+      <input type="text" name = ""  onChange={changeInputData}/>
+      <AppBlock>
+        <Button onClick={addTodo}>Button</Button>
+      </AppBlock>
+    </form>
+
+    <List todos={todos}/>
+    </>
   );
 }
 
